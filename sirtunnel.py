01c4a51d514364b6eda252c77a664eba050a9676
AviiNL <yj.iedema@gmail.com>
2021-09-04 13:44:19 +0200

    hopefully error handling

--- sirtunnel.py
+++ sirtunnel.py
@@ -13,10 +13,13 @@ def delete(tunnel_id, caddy_api):
     req = request.Request(method='DELETE', url=delete_url)
     request.urlopen(req)
 
+    delete_error_url = '{}/id/{}'.format(caddy_api, tunnel_id + '-error')
+    error_req = request.Request(method='DELETE', url=delete_error_url)
+    request.urlopen(error_req)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("SirTunnel", description="An easy way to securely expose a webserver running on one computer via a public URL")
     parser.add_argument("--debug", action="store_true", help="Additional logs")
-    parser.add_argument("--check", action="store_true", help="Check each second whether entry still exists")
     parser.add_argument("--caddy-api", default="http://127.0.0.1:2019", help="Caddy's admin api")
     parser.add_argument("domain", help="External domain name")
     parser.add_argument("tunnel_port", type=int, help="The tunnel port between your computer and the server per the command ssh -tr <tunnel_port>:localhost:8000")
@@ -62,11 +65,27 @@ if __name__ == '__main__':
         }]
     }
 
-    body = json.dumps(caddy_add_route_request).encode('utf-8')
+    caddy_add_error_request = {
+        "@id": tunnel_id + "-error",
+        "match": [{
+            "host": [host],
+        }],
+        "handle": [{
+            "handler": "static_response",
+            "body": "Tunnel " + host + " is unreachable."
+        }]
+    }
+
+    route_body = json.dumps(caddy_add_route_request).encode('utf-8')
+    error_body = json.dumps(caddy_add_error_request).encode('utf-8')
     
-    create_url = '{}/config/apps/http/servers/sirtunnel/routes'.format(caddy_api)
-    req = request.Request(method='POST', url=create_url, headers=headers)
-    request.urlopen(req, body)
+    create_route_url = '{}/config/apps/http/servers/sirtunnel/routes'.format(caddy_api)
+    route_req = request.Request(method='POST', url=create_route_url, headers=headers)
+    request.urlopen(route_req, route_body)
+
+    create_error_url = '{}/config/apps/http/servers/sirtunnel/errors/routes'.format(caddy_api)
+    error_req = request.Request(method='POST', url=create_error_url, headers=headers)
+    request.urlopen(error_req, error_body)
 
     print("Tunnel created successfully")
 
@@ -74,9 +93,8 @@ if __name__ == '__main__':
         try:
             time.sleep(1)
             # Quick check that the tunnel still exists
-            if args.check:
-                req = request.Request(method='GET', url=caddy_api + '/id/' + tunnel_id)
-                response = request.urlopen(req)
+            req = request.Request(method='GET', url=caddy_api + '/id/' + tunnel_id)
+            response = request.urlopen(req)
         except KeyboardInterrupt:
             print("Cleaning up tunnel")
             delete_url = caddy_api + '/id/' + tunnel_id
